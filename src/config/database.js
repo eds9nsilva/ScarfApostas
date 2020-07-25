0509acc6ea9eb8dde9e393a97dbdc29566c6cd39
eds9nsilva <antonioedson99@gmail.com>
2020-07-25 01:18:22 -0300

    backend 0.1

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: '3306',
+    username: 'root',
+    password: '',
+    database: 'aposta',
+    define: {
+        timestamps: true,
+        underscored: true,
+        timezone: 'BRT:Brasília'
+    }
+}
