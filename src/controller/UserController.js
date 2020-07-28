8515b0554f955a903055e2866eae2c0926f4bc18
eds9nsilva <antonioedson99@gmail.com>
2020-07-28 20:47:37 -0300

    backend 0.3

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,15 +1,40 @@
 const User = require('../models/User');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
 module.exports = {
-    async Store(req, res){
+    async Store(req, res) {
         try {
-            const {name, email, password} = req.body;
+            const { name, email, password } = req.body;
             seach = await User.findOne({ where: { email: email } });
-            if(seach != null){
-                return res.json({Message: "Email já cadastrado"})
+            if (seach == null) {
+                return res.json({ Message: "Email já cadastrado" })
+            } else {
+                await User.create({ name, email, password }).then(function (result) {
+                    return res.status(200).json(result);
+                })
+                const transporter = await nodemailer.createTransport({
+                    service: 'Gmail',
+                    auth: {
+                        user: "eds9ndasilva@gmail.com",
+                        pass: "phpsantosfc007"
+                    }
+                });
+                transporter.use('compile', hbs({
+                    viewEngine: 'express-handlebars',
+                    viewPath: '/src/controller/'
+                }));
+                
+                const emailOptions = {
+                    from: 'eds9ndasilva@gmail.com',
+                    to: email,
+                    subject: 'Seja Bem-vindo',
+                    template: 'index'
+                }
+                transporter.sendMail(emailOptions, function(err){
+                    console.log(err)
+                })
             }
-            await User.create({name,email,password}).then(function(result){
-                return res.status(200).json(result);
-            })
         } catch (error) {
             return res.status(400).json({ error: error });
         }
---
+++ src/utils/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+/*
+module.exports = {
+    
+
+    /*async sendRegister(email){
+        const  e = email;
+        console.log(e);
+        /*const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: "eds9ndasilva@gmail.com",
+                pass: "phpsantosfc007"
+            }
+        });
+        transporter.sendMail({
+            from: 'eds9ndasilva@gmail.com',
+            to: email,
+            subject: 'hello world',
+            text: 'hello world!'
+        })
+    }
+}
+
+module.exports = sendRegister()*/
